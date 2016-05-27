168d1d6cfd7a2607075b279c8c00ff1166fa06b5
Porraryd <pontusorraryd@gmail.com>
2016-05-26 17:09:48 -0700

    upload custom file

--- app/main.js
+++ app/main.js
@@ -1,17 +1,18 @@
 
 define(function (require) {
 
+    var fileHandler = require('./fileHandler.js');
     var table = require('./tableDrawer.js');
     var categoryDashboard = require('./categoryDashboard.js');
 
-    categories = table.initData("sample_data_sales.csv", function(data, fields) {
+    
+    fileHandler.setFileCallback( function (data) {
+        var fields = table.initData(data)
         categoryDashboard.createDashboard("category-dashboard", fields, table);
         var header = document.getElementById("table_header");
-        
         var late = fields[Object.keys(data[0])[0]].displayName
         header.innerHTML =  "Showing data by " + late
     });
-
 
 
 });
